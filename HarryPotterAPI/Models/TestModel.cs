62a72b79a8874edd8afdca3d158c0c18da7e033f
Dem-Ivan <demidenko.vanya@list.ru>
2021-04-13 20:20:53 +0300

    Wos eded the model TestModel

---
+++ HarryPotterAPI/Models/TestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarryPotterAPI.Models
+{
+	public class TestModel
+	{
+		public string Prop_1 { get; set; }
+		public string Prop_2 { get; set; }
+		public string Prop_3 { get; set; }
+
+	}
+}
