9249cb4acd413aeef55ec2859b46083f8b3c3ad3
Dem-Ivan <demidenko.vanya@list.ru>
2021-04-13 20:45:22 +0300

    Wos chenged the type of property of TestModel

---
+++ HarryPotterAPI/Models/TestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HarryPotterAPI.Models
+{
+	public class TestModel
+	{
+		public int Prop_1 { get; set; }
+		public int Prop_2 { get; set; }
+		public int Prop_3 { get; set; }
+
+	}
+}
